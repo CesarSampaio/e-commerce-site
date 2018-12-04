4e4302e623d009bed546d55917f86a23fbaa7bb0
CesarSampaio <c.sampaiosan@gmail.com>
2018-12-03 23:07:02 -0400

    front page ready

--- includes/php/categories-slick-slider.php
+++ includes/php/categories-slick-slider.php
@@ -1,79 +1,49 @@
 
     <?php
-        /*$row="";*/
-        $i = 0;
-
-        /*$resultArray = array();
-        $resultlog = mysqli_query($con, "SELECT * from product_categories");
-            while($row = mysqli_fetch_array($resultlog)) {
-                $resultArray[] = $row;
-            }
-        print_r($resultArray);
-
-        echo "<br>";*/
+        $k = 0;
 
         $resultArray02 = array();
-        $css_query_01 = "select * from categories where cat_title='Moda Praia'";
+        $css_query_01 = "select * from categories where cat_title = '" . $index_resultArray01[$i]['cat_title'] . "'";
+        /*echo $css_query_01; echo "<br>";*/
         $css_result_01 = mysqli_query($con, $css_query_01);
         while($row = mysqli_fetch_array($css_result_01)) {
             $lt = $row['cat_id'];
             $an = $row['cat_title'];
-                /*echo "<script>alert(".$lt.")</script>";
-                echo $lt; echo "<br>";
-                echo $an;
-                $resultArray02=$row;*/
         }
 
-        /*print_r($resultArray02);*/
-
-        /*echo "<br>";echo "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccc";*/
-        
         $resultArray03 = array();
-        $css_query_02 = "select * from products where cat_id = " . $lt;
-        /*echo $css_query_02;echo "<br>";echo "*************************************************";*/
+        $css_query_02 = "select * from products where p_cat_id = '" . $index_resultArray02[$j]['p_cat_id'] . "'";
+        /*echo $css_query_02; echo "<br>";*/
         $css_result_02 = mysqli_query($con, $css_query_02);
-        /*print_r($css_result_02);echo "<br>";echo "*************************************************";*/
         $num_rows_array = mysqli_num_rows($css_result_02);
-        /*echo $num_rows_array;echo "<br>";echo "*************************************************";*/
+        /*echo "Num de Rows: ";echo $num_rows_array;echo "<br>";echo "*************************************************";*/
         while($row = mysqli_fetch_array($css_result_02)) {
             $resultArray03[] = $row;
         }
-            /*print_r($resultArray03);echo "<br>";echo "cccccccccccccccccccccccccccccccccccccccccccccccccccccccc";
-            echo implode(', ', $resultArray03[0]);
-            echo "<br><br>";
-            echo $resultArray03[0][0];echo "<br>";
-            echo $resultArray03[0]['product_id'];echo "<br>";
-            echo $resultArray03[0]['product_features'];echo "<br>";
-            echo $resultArray03[0]['product_keywords'];echo "<br>";
-            echo $resultArray03[0]['product_label'];echo "<br>";
-            echo $resultArray03[0]['product_type'];echo "<br>";
-            echo $resultArray03[0]['product_img1'];echo "<br>";
-            echo $resultArray03[0]['product_img2'];echo "<br>";
-            echo $resultArray03[0]['product_title'];echo "<br>";
-            echo $resultArray03[0]['product_price'];echo "<br>";*/
+        echo $i;echo"<br>";echo $j;echo"<br>";
     ?>
     
     <div class="slick-slider-container">
-        <div class="slick-slider-title"><?php echo $an ?></div>
+        <div class="slick-slider-title"><?php echo $index_resultArray02[$j]['p_cat_title'] ?></div>
             <div class="slider2">
                 <?php
-                    while($i < $num_rows_array) {
+                    while($k < $num_rows_array) {
                         echo '              <!--/****************************************************************************************************/-->';
                         echo '              <div class="abc">';
                         echo '                  <div class="product-slider">';
                         echo '                      <div class="product-slider-wrapper">';
                         echo '                          <div class="product-slider-card">';
                         echo '                              <div class="product-slider-image-container">';
-                        echo '                                  <img src="/product_images/'; echo $resultArray03[$i]['product_img1']; echo '" alt="" class="product-img-card">';
+                        echo '                                  <img src="/product_images/'; echo $resultArray03[$k]['product_img1']; echo '" alt="" class="product-img-card">';
                         echo '                              </div>';
                         echo '                              <i class="fas fa-heart fa-3x product-slider-icon"></i>';
-                        echo '                              <p class="product-slider-nome">'; echo $resultArray03[$i]['product_title']; echo '</p>';
-                        echo '                              <p class="product-slider-price">'; echo $resultArray03[$i]['product_price']; echo '</p>';
+                        echo '                              <p class="product-slider-nome">'; echo $resultArray03[$k]['product_title']; echo '</p>';
+                        echo '                              <p class="product-slider-price">R$ '; echo $resultArray03[$k]['product_price']; echo '</p>';
                         echo '                          </div>';
                         echo '                      </div>';
                         echo '                  </div>';
                         echo '              </div>';
-                        $i++;
+                        $k++;
                     }
                 ?>
             </div>
--- index.php
+++ index.php
@@ -6,9 +6,62 @@
     include "includes/php/carousel.php";
     include "includes/php/promo.php";
     include "includes/php/banners.php";
-    include "includes/php/categories-slick-slider.php";
-    include "includes/php/categories-slick-slider.php";
-    include "includes/php/categories-slick-slider.php";
+
+    $index_resultArray01 = array();
+    $index_resultArray02 = array();
+
+    $index_query_01 = "select * from categories where cat_id >= 8";
+    $index_result_01 = mysqli_query($con, $index_query_01);
+    $index_num_rows_01 = mysqli_num_rows($index_result_01);
+    while($row1 = mysqli_fetch_array($index_result_01)) {
+        $index_resultArray01[] = $row1;
+        $index_query_02 = "select * from product_categories where p_cat_top = " . $row1['cat_id'];
+        /*echo "<script>$index_query_02</script>";*/
+        $index_result_02 = mysqli_query($con, $index_query_02);
+        $index_num_rows_02 = mysqli_num_rows($index_result_02);
+        while($row2 = mysqli_fetch_array($index_result_02)) {
+            $index_resultArray02[] = $row2;
+        }
+    }
+    /*print_r($index_resultArray01);
+    print_r($index_resultArray02);*/
+
+    /*echo"<br>";*/
+
+    /*echo $index_resultArray01[0]['cat_title'];echo "<br>";
+    echo $index_resultArray02[0]['p_cat_title'];echo "<br>";
+    echo $index_resultArray02[1]['p_cat_title'];echo "<br>";
+
+    echo $index_resultArray02[0]['p_cat_id'];echo "<br>";
+    echo $index_resultArray02[1]['p_cat_id'];echo "<br>";
+
+    echo $index_resultArray01[1]['cat_title'];echo "<br>";
+    echo $index_resultArray02[2]['p_cat_title'];echo "<br>";
+    echo $index_resultArray02[3]['p_cat_title'];echo "<br>";
+
+    echo $index_resultArray02[2]['p_cat_id'];echo "<br>";
+    echo $index_resultArray02[3]['p_cat_id'];echo "<br>";
+
+    echo $index_num_rows_01; echo "<br>";
+    echo $index_num_rows_02; echo "<br>";*/
+
+        /*for($i=0; $i<$index_num_rows_01;$i++) {
+            echo $i; echo "<br>";
+            for($j=0; $j<(sizeof($index_resultArray02));$j++) {
+                echo ".      ."; echo $j; echo "<br>"; 
+            }
+        }*/
+
+        for($i=0; $i<$index_num_rows_01;$i++) {
+            /*echo $i;echo "<br>";*/
+            /*echo $index_resultArray01[$i]['cat_title'];echo "<br>";*/
+            for($j=($i*2); $j<(($i*2)+2);$j++) {
+                /*echo $j;echo "<br>";
+                echo $index_resultArray02[$j]['p_cat_title'];echo "<br>";*/
+                include "includes/php/categories-slick-slider.php";
+            }
+        }
+    
     include "includes/php/instagram.php";
     include "includes/php/footer.php";
 ?>
